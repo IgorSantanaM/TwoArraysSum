b0180a021ea4c1351b82a1465e0ee34c8ce96795
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-28 20:02:47 -0400

    Initial commit

---
+++ TwoArraysSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TwoArraysSum; 
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] result = TheSumOfArrays.ArraySum(); 
+
+        Console.WriteLine("The sum of the two arrays is:");
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
